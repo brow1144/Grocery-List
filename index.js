605fbd87b83a2802dda5d05ffbfca836922d2047
Kyle Brown <brow1144@purdue.edu>
2017-06-06 19:47:38 -0400

    Add items to top of the list

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const itemForm = document.querySelector('#itemForm')
+let c = 0
+let itemArray = []
 
 function renderItem(item) {
-    const newItem = document.createElement('p')
+    const newItem = document.createElement('li')
     newItem.innerHTML = `${item}`
-    
+
     return newItem
 }
 
+function renderList(item) {
+    const list = document.createElement('ul')
+
+    for (let i = 0; i < itemArray.length; i++) {
+        list.appendChild(renderItem(itemArray[i]))
+    }
+    return list
+}
+
 function handleAdd(e) {
-  e.preventDefault()
-  const listItem = e.target
+    e.preventDefault()
 
-  const div = document.querySelector('#enterItem')
+    if(c == 1) {
+    const x = document.querySelector('ul')
+    x.remove() 
+    }
 
-  div.appendChild(renderItem(listItem.groceryItem.value))
-  
+    const listItem = e.target
+
+    itemArray.unshift(listItem.groceryItem.value)
+
+    itemForm.appendChild(renderList(listItem.groceryItem.value))
+
+    //itemForm.appendChild(renderItem(listItem.groceryItem.value))
+    listItem.reset()
+    c = 1
 }
 
 itemForm.addEventListener('submit', handleAdd)

4aa7356ffe034eb3262b2dbbe13d956bd2c92419
Kyle Brown <brow1144@purdue.edu>
2017-06-07 00:45:45 -0400

    Add favorite button with background color change
    Need to fix the problem that when a new item is added all the favorites
    go away. Not huge problem right now

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ let itemArray = []
 
 function renderItem(item) {
     const newItem = document.createElement('li')
+    //Removes Spaces
+    const noSpace = item.replace(/\s/g, '');
+    //Make Label the Entered Item
     newItem.innerHTML = `${item}`
+    const favoriteButton = document.createElement('button')
+    favoriteButton.setAttribute('type', 'button')
+    //Give button id with no spaces to find which to highlight 
+    favoriteButton.setAttribute(`id`, `${noSpace}`)
+    favoriteButton.textContent = 'Favorite'
+    newItem.appendChild(favoriteButton)
+
+    favoriteButton.addEventListener('click', handleFavorite)
 
     return newItem
 }
@@ -15,21 +26,50 @@ function renderList(item) {
     for (let i = 0; i < itemArray.length; i++) {
         list.appendChild(renderItem(itemArray[i]))
     }
+
     return list
+}
+
+function validateNewItem(value) {
+    for(let i = 0; i < itemArray.length; i++) {
+        if(itemArray[i] == value)   {
+            alert('That item is already in the list!')
+            return false
+        }
+    }
+    return true
 }
 
 function handleAdd(e) {
     e.preventDefault()
     const listItem = e.target
 
+    //TODO Check if item is already in list 
+    if(validateNewItem(listItem.groceryItem.value) == false) {
+        return false
+    }
+
     if(itemArray.length > 0) {
         const x = document.querySelector('ul')
         x.remove() 
     }
-
+    
     itemArray.unshift(listItem.groceryItem.value)
     itemForm.appendChild(renderList(listItem.groceryItem.value))
     listItem.reset()
+}
+
+function handleFavorite(e) {
+    e.preventDefault()
+        
+    const background = document.querySelector(`#${e.target.id}`)
+    const parent = background.parentElement
+
+    if (parent.style.backgroundColor == 'lightgreen') {
+        parent.style.backgroundColor = 'whitesmoke'
+    } else {            
+        parent.style.backgroundColor = 'lightgreen'
+    }
 }
 
 itemForm.addEventListener('submit', handleAdd)
